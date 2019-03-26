84dab153301b68e5aac71baebf3991e0c122c48a
Tobias Pritsch <tobias.pritsch@cosee.biz>
2019-03-26 10:49:53 +0100

    Added dependencies; Added time formatter; added DateTime clone method

---
+++ lib/util/clone.dart
@@ -0,0 +1,3 @@
+
+DateTime cloneDT(DateTime d) =>
+    DateTime(d.year, d.month, d.day, d.hour, d.minute, d.second);
---
+++ lib/util/formatter.dart
@@ -0,0 +1,6 @@
+import 'package:intl/intl.dart';
+
+var dateFormatter = DateFormat('dd.MM.yyyy');
+var clockFormatter = DateFormat('hh:mm');
+var fullDateFormatter = DateFormat('EE, dd.MM.yyyy');
+var weekDayFormatter = DateFormat('EE');
